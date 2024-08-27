6b8c4595840b6d64ddafb878292e2800f0e550a6
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2024-08-28 08:42:50 +1000

    add distributed training script

---
+++ distributed_train_bw_all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#SBATCH --job-name=distributed_pretrain_film                       # Job name
+#SBATCH --nodes=4
+#SBATCH --output=./training/result-%j.out            # Standard output and error log
+#SBATCH --error=./training/error-%j.err              # Error file
+#SBATCH --partition=OOD_gpu_32gb         # Specify the GPU partition
+#SBATCH --gres=gpu:2                      # Request 2 GPUs
+#SBATCH --mem=191844                         # Max memory available for node
+#SBATCH --ntasks=1                        # Run on a single CPU
+#SBATCH --cpus-per-task=16                 # Number of CPU cores per task
+#SBATCH --ntasks-per-node=2
+
+hostname
+
+module add cuda/11.1
+module add python3/anaconda/2020.02
+source activate deepsolo
+
+conda list
+module list
+which python
+python --version
+
+# Set the master node and its port
+export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
+export MASTER_PORT=12345
+
+# Number of nodes and GPUs per node
+NUM_NODES=$SLURM_NNODES
+NUM_GPUS_PER_NODE=2
+
+# Total number of GPUs (world size)
+WORLD_SIZE=$(($NUM_NODES * $NUM_GPUS_PER_NODE))
+
+# Set the necessary environment variables for torch.distributed
+export NCCL_DEBUG=INFO
+export NCCL_SOCKET_IFNAME=^docker0,lo
+
+# Run the distributed training script
+srun python -m torch.distributed.launch \
+    --nproc_per_node=$NUM_GPUS_PER_NODE \
+    --nnodes=$NUM_NODES \
+    --node_rank=$SLURM_NODEID \
+    --master_addr=$MASTER_ADDR \
+    --master_port=$MASTER_PORT \
+    tools/train_net.py \
+    --config-file configs/R_50/film/train_bw.yaml \
+    --num-gpus $WORLD_SIZE \
+    --num-machines $NUM_NODES \
+    --machine-rank $SLURM_NODEID \
+    SOLVER.IMS_PER_BATCH 32
