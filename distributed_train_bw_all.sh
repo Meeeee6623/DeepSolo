59a02b9a7b94a9d270672ee652eded40c71adc4e
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2024-08-28 15:22:13 +1000

    use only detectron2 for distribution

--- distributed_train_bw_all.sh
+++ distributed_train_bw_all.sh
@@ -22,7 +22,7 @@ which python
 python --version
 
 # Set the master node and its port
-export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
+export MASTER_ADDR=$(scontrol show hostname "$SLURM_NODELIST" | head -n 1)
 export MASTER_PORT=14567
 
 # Set OMP_NUM_THREADS
@@ -32,33 +32,26 @@ export OMP_NUM_THREADS=16 # 16 CPU cores per task
 NUM_NODES=$SLURM_NNODES
 NUM_GPUS_PER_NODE=2
 
-# Total number of GPUs (world size)
-WORLD_SIZE=$(($NUM_NODES * $NUM_GPUS_PER_NODE))
-
 # Set the necessary environment variables for torch.distributed
 export NCCL_DEBUG=INFO
 export NCCL_SOCKET_IFNAME=^docker0,lo
 
 # Run the distributed training script on each node independently
-for NODE_RANK in $(seq 0 $(($NUM_NODES - 1)))
+for NODE_RANK in $(seq 0 $((NUM_NODES - 1)))
 do
     echo "SLURM_PROCID: $SLURM_PROCID"
     echo "SLURM_NTASKS: $SLURM_NTASKS"
     echo "SLURM_NODEID: $SLURM_NODEID"
 
-    srun --nodes=1 --ntasks=1 --exclusive -w "$(scontrol show hostname $SLURM_NODELIST | sed -n "$(($NODE_RANK + 1))p")" \
-        --output=./training/job_${SLURM_JOB_ID}_node_${NODE_RANK}.out \
-        --error=./training/job_${SLURM_JOB_ID}_node_${NODE_RANK}.err \
-        conda run python -m torch.distributed.run \
-        --nproc_per_node=$NUM_GPUS_PER_NODE \
-        --nnodes="$NUM_NODES" \
-        --node_rank="$NODE_RANK" \
-        --rdzv_backend=c10d \
-        --rdzv_endpoint="$MASTER_ADDR:$MASTER_PORT" \
-        --rdzv_id="$SLURM_JOB_ID" \
-        tools/train_net.py \
+    srun --nodes=1 --ntasks=1 --exclusive -w "$(scontrol show hostname "$SLURM_NODELIST" | sed -n "$((NODE_RANK + 1))p")" \
+        --output=./training/job_"${SLURM_JOB_ID}"_node_"${NODE_RANK}".out \
+        --error=./training/job_"${SLURM_JOB_ID}"_node_"${NODE_RANK}".err \
+        conda run python tools/train_net.py \
         --config-file configs/R_50/film/train_bw.yaml \
-        --num-gpus="$WORLD_SIZE" \
+        --num-gpus="$NUM_GPUS_PER_NODE" \
+        --machine_rank="$NODE_RANK" \
+        --num_machines="$SLURM_NTASKS" \
+        --dist-url "tcp://$MASTER_ADDR:$MASTER" \
         --resume \
         --opts SOLVER.IMS_PER_BATCH 16 &
 done
